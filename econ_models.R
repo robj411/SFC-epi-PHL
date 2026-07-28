5bcbe31dccb7ec130df71eacfee22fcd5fa882d0
Rob Johnson <robj411@users.noreply.github.com>
2026-07-28 09:57:56 +0800

    update parameters

--- econ_models.R
+++ econ_models.R
@@ -17,14 +17,16 @@ h0    = 19032.66/1e3   # in billions
 model1$econ_init = h0
 model1$G      = g0                     # government spending (constant)
 model1$theta  = theta                  # tax rate on income
-model1$alpha1 = 0.6839286/.9          # propensity to consume out of income
-model1$alpha2 = 0.005339403/365       # propensity to consume out of wealth (daily)
+model1$alpha1 = 0.7695          # propensity to consume out of income
+model1$alpha2 = 0.0105*4/365       # propensity to consume out of wealth (daily)
 model1$alpha0 = with(model1, cons0 - h0*alpha2 - yd0*alpha1)
 print(model1$alpha0)
 model1$gdp    = y0*365
 model1$y0     = model1$gdp/365
+##!! change to people
 model1$lf     = ldata$NNs[1]/1e6     # labour force in millions
 model1$emp0   = model1$lf*ldata$employmentrate/100
+##!! change to per million people
 model1$lambda = model1$y0/model1$emp0
 model1$cons0  = cons0
 
